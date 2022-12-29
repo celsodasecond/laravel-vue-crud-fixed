68a010b27bdf54420d34e05614727b9a40c7e67d
celsodasecond <75917932+celsodasecond@users.noreply.github.com>
2022-12-29 20:33:22 +0800

    feat: Pagination (6 per Page)

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -237,12 +237,13 @@ const store = createStore({
         },
         surveys: {
             loading: false,
+            links: [],
             data: []
         },
         questionTypes: ["text", "select", "radio", "checbox", "textarea"],
         notification: {
             show: false,
-            type:null,
+            type: null,
             message: null
         }
     },
@@ -284,9 +285,10 @@ const store = createStore({
         deleteSurvey({ }, id) {
             return axiosClient.delete(`/survey/${id}`);
         },
-        getSurveys({ commit }) {
+        getSurveys({ commit }, { url = null } = {}) {
+            url = url || '/survey'
             commit('setSurveysLoading', true)
-            return axiosClient.get("/survey").then((res) => {
+            return axiosClient.get(url).then((res) => {
                 commit('setSurveysLoading', false)
                 commit("setSurveys", res.data);
                 return res;
@@ -325,6 +327,7 @@ const store = createStore({
             state.currentSurvey.data = survey.data;
         },
         setSurveys: (state, surveys) => {
+            state.surveys.links = surveys.meta.links;
             state.surveys.data = surveys.data;
         },
         logout: state => {
@@ -336,7 +339,7 @@ const store = createStore({
             state.user.data = userData.user;
             sessionStorage.setItem('TOKEN', userData.token);
         },
-        notify: (state, {message, type}) => {
+        notify: (state, { message, type }) => {
             state.notification.show = true;
             state.notification.type = type;
             state.notification.message = message;
--- vue/src/views/Surveys.vue
+++ vue/src/views/Surveys.vue
@@ -2,7 +2,8 @@
     <PageComponent>
         <template v-slot:header>
             <div class="flex justify-between items-center">
-                <h1 class="text-3xl font-bold tracking-tight text-gray-900 animate__animated animate__fadeInLeft">Surveys</h1>
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900 animate__animated animate__fadeInLeft">
+                    Surveys</h1>
                 <router-link :to="{ name: 'SurveyCreate' }"
                     class="py-2 px-3 text-white bg-emerald-500 rounded-md hover:bg-emerald-600">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
@@ -16,20 +17,43 @@
         </template>
 
         <div v-if="surveys.loading" class="flex justify-center">
-        <div class="flex items-center justify-center space-x-2">
-            <div class="w-4 h-4 rounded-full animate-pulse dark:bg-gray-800"></div>
-            <div class="w-4 h-4 rounded-full animate-pulse dark:bg-gray-800"></div>
-            <div class="w-4 h-4 rounded-full animate-pulse dark:bg-gray-800"></div>
+            <div class="flex items-center justify-center space-x-2">
+                <div class="w-4 h-4 rounded-full animate-pulse dark:bg-gray-800"></div>
+                <div class="w-4 h-4 rounded-full animate-pulse dark:bg-gray-800"></div>
+                <div class="w-4 h-4 rounded-full animate-pulse dark:bg-gray-800"></div>
+            </div>
         </div>
+        <div v-else>
+            <div class="grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3 animate__animated animate__slideInUp">
+                <SurveyListItem v-for="survey in surveys.data" :key="survey.id" :survey="survey"
+                    @delete="deleteSurvey(survey)">
+                </SurveyListItem>
+            </div>
+            
+            <div class="flex justify-center mt-5 animate__animated animate__backInUp">
+                <nav class="relative z-0 inline-flex justify-center rounded-md shadow-sm" aria-label="Pagination">
+                    <a 
+                    v-for="(link, i) of surveys.links" 
+                    :key="i" 
+                    :disabled="!link.url" 
+                    v-html="link.label"
+                    href="#"
+                    @click="getForPage($event, link)"
+                    aria-current="page"
+                    class="relative inline-flex items-center px-4 py-2 border text-sm font-medium whitespace-nowrap"
+                    :class="[
+                        link.active
+                        ? 'z-10 bg-indigo-50 border-indigo-500 text-indigo-600'
+                        : 'bg-white border-gray-300 text-gray-500 hover:bg-gray-50',
+                        i === 0 ? 'rounded-1-md' : '',
+                        i === surveys.links.length - 1 ? 'rounded-r-md' : '',
+                    ]">
+
+                    </a>
+                </nav>
+            </div>
         </div>
-        <div v-else class="grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3 animate__animated animate__slideInUp">
-            <SurveyListItem 
-            v-for="survey in surveys.data" 
-            :key="survey.id" 
-            :survey="survey"
-            @delete="deleteSurvey(survey)"
-            ></SurveyListItem>
-        </div>
+        
     </PageComponent>
 </template>
 
@@ -46,9 +70,20 @@ store.dispatch('getSurveys')
 function deleteSurvey(survey) {
     if (confirm(`Are you sure you really want to delete this survey? This Operation is cannot be undone.`)) {
         store.dispatch('deleteSurvey', survey.id)
-        .then(() => {
-            store.dispatch('getSurveys')
-        })
+            .then(() => {
+                store.dispatch('getSurveys')
+            })
     }
 }
+
+function getForPage(ev, link) {
+    ev.preventDefault();
+    
+    if (!link.url || link.active) {
+        return;
+    }
+
+    store.dispatch("getSurveys", {url: link.url});
+}
+
 </script>
